bd01dc7f40142001d9c15bc2a3ea4380a7324ce1
kizitoinegbenose@gmail.com <decagon@Decagons-MacBook-Air.local>
2021-04-20 20:40:46 +0100

    complete task

--- task-one/src/app.js
+++ task-one/src/app.js
@@ -3,7 +3,17 @@ import FileTree from './fileTree';
 export function createFileTree(input) {
   const fileTree = new FileTree();
 
-  for (const inputNode of input) {
+  const copy = JSON.parse(JSON.stringify(input));
+  const wellOrdered = copy.splice(0,1);
+  let parentId = wellOrdered[0].id;
+
+  for (let _ of copy) {
+    const childNode = copy.find(el => el.parentId === parentId);
+    wellOrdered.push(childNode);
+    parentId = childNode.id;
+  }
+
+  for (const inputNode of wellOrdered) {
     const parentNode = inputNode.parentId
       ? fileTree.findNodeById(inputNode.parentId)
       : null;
